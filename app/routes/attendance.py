96f0457258827ab33d3af76d0ee776f96a3b2186
Ching Lam Lau <contact.minicube11@gmail.com>
2022-08-27 22:50:31 -0400

    Add views for displaying code and attendees

--- app/routes/attendance.py
+++ app/routes/attendance.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from app import db
 from app.forms import AttendanceForm, CreateAttendanceForm
@@ -17,8 +17,14 @@ def attendance_redirect():
 def index():
     form = AttendanceForm()
     if form.validate_on_submit():
-        attendance = Attendance.query.filter_by(code=form.code.data).first()
-        if attendance:
+        code = form.code.data
+    else:
+        code = request.args.get('code')
+    print(code)
+    if code:
+        attendance = Attendance.query.filter_by(code=code).first()
+        current_time = datetime.datetime.utcnow()
+        if attendance and attendance.start_time <= current_time <= attendance.end_time:
             attendance.attendees.append(current_user)
             db.session.commit()
             return redirect(url_for('attendance.attendance_success'))
@@ -29,7 +35,7 @@ def index():
 def attendance_success():
     return "The code you submitted was valid."
 
-@bp.route('/attendance/admin',methods=['GET', 'POST'])
+@bp.route('/attendance/admin', methods=['GET', 'POST'])
 @login_required
 def attendance_admin():
     form = CreateAttendanceForm()
@@ -42,6 +48,17 @@ def attendance_admin():
         db.session.add(attendance)
         db.session.commit()
         return redirect(url_for('attendance.attendance_admin'))
-    attendances = current_user.attendance
-    print(attendances)
+    attendances = Attendance.query.all()
     return render_template('attendance/admin.html', form=form, attendances=attendances)
+
+@bp.route('/attendance/<code>/attendees')
+@login_required
+def attendees(code):
+    attendance = Attendance.query.filter_by(code=code).first_or_404()
+    return render_template('attendance/attendees.html', attendance=attendance)
+
+@bp.route('/attendance/<code>/display')
+@login_required
+def display(code):
+    attendance = Attendance.query.filter_by(code=code).first_or_404()
+    return render_template('attendance/display.html', attendance=attendance)
