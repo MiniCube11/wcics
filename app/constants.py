dc597f9652da3f14d0ddcc369adbce2a9450fc9d
Ching Lam Lau <contact.minicube11@gmail.com>
2022-09-15 17:09:37 -0400

    Update faq and meeting time on home page

---
+++ app/constants.py
@@ -0,0 +1,14 @@
+faq = [
+    {
+        "question": "What if I don’t know how to code?",
+        "answer": "We will be teaching the basics of coding in the beginner class, so you don’t need any previous knowledge to attend.",
+    },
+    {
+        "question": "Will the meetings be in-person or online?",
+        "answer": "Our meetings will be in-person this year!",
+    },
+    {
+        "question": "Can I still join now?",
+        "answer": "You’re welcome to join our meetings at any time of the year :)",
+    },
+]
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
+from app.constants import faq
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('main/index.html', home=True)
+    return render_template('main/index.html', faq=faq, home=True)
