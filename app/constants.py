8f2c2e3c7700c2b424f1ed6b1c010477e3b181b0
Ching Lam Lau <contact.minicube11@gmail.com>
2023-09-13 22:51:15 -0400

    Update website info

--- app/constants.py
+++ app/constants.py
@@ -11,4 +11,8 @@ faq = [
         "question": "Can I still join now?",
         "answer": "You’re welcome to join our meetings at any time of the year :)",
     },
+    {
+        "question": "How can I receive announcements or updates?",
+        "answer": "We’ll be posting announcements about meetings, events, and opportunities in our Google Classroom (join code: c2prjk3). You can also follow our Instagram (@wci_computer_science) to get updates for big events throughout the year.",
+    },
 ]
