77b1c4a6cfaad082c9c22cb3ebc3b594eb37c709
Ching Lam Lau <contact.minicube11@gmail.com>
2024-09-22 18:21:29 -0400

    Add autoincrement to user model id

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ user_attendance = db.Table('user_attendance',
                             db.Column('attendance_id', db.Integer, db.ForeignKey('attendance.id')))
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
